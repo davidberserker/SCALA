cfef156dff7a5797d6b7b085a7b9099a0d509ebb
Ignacio Jimenez <ignacio.jimenez@solera.com>
2022-12-16 02:20:47 -0600

    Added the basics of programming oriented object and its examples

--- udemy-scala-beginners 1.0/src/main/scala/lectures/part2oop/OOBasics.scala
+++ udemy-scala-beginners 1.0/src/main/scala/lectures/part2oop/OOBasics.scala
@@ -1,10 +1,26 @@
 package lectures.part2oop
 
+import org.w3c.dom.css
+import org.w3c.dom.css.Counter
+
 object OOBasics extends App{
 
   val person = new Person("John",26)
   println(person.age)
   person.greet("Daniel")
+
+  val author = new Writer("Charles", "Dickens", 1812)
+  val imposter = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great Expectations", 1861, author)
+
+  println("The author novel age is: " + novel.authorAge)
+  println(novel.isWrittenBy(author))
+  println(novel.isWrittenBy(imposter))
+
+  val counter = new Counter ()
+  counter.inc.print
+  counter.inc.inc.inc.print
+  counter.inc(10).print
 }
 
 //Class constructor () like the next code
@@ -24,9 +40,75 @@ object OOBasics extends App{
   def this (name:String) = this(name,0)
   def this() = this ("John Doe")
 
+  //Class parameters are NOT FIELDS
+  //Class parameters and class fields are not the same
+  //To convert a parameter into a field you should use VAL
+
 
 }
 
-//Class parameters are NOT FIELDS
-//Class parameters and class fields are not the same
-//To convert a parameter into a field you should use VAL
+/*
+
+  Exercise
+  create novel and a writer
+
+writer: first name, surname , year
+- method fullname
+
+Novel: name, year of release, author
+-authorAge
+-iswrittenBy(author)
+-copy (new year of release) = new instance of Novel
+
+
+*/
+
+class Writer (val name: String, val surname: String,val year: Int){
+
+  def fullName:  String = name + " " + surname
+}
+
+class Novel (val name: String, val year: Int, val author:Writer){
+
+  def authorAge = year - author.year
+  def isWrittenBy (author: Writer) = author ==this.author
+  def copy(newYear: Int): Novel = new Novel (name, newYear, author)
+}
+
+
+/*
+Counter class
+- Receives an int value
+- Method current count
+- Method to increment/decrement => new counter
+- Overload inc/dec to receive an amount
+* */
+
+class Counter ( val count: Int = 0){
+  def inc = {
+    println("Incrementing...")
+    new Counter (count + 1)
+  } //inmutability
+  def dec = {
+    println("Decrementing...")
+    new Counter (count - 1)
+  }
+
+  //Overload of methods
+//  def  inc (n: Int) = new Counter (count + n)
+//  def  dec (n: Int) = new Counter (count - n)
+//Instead of lloping we want recursion
+
+  def inc(n: Int) : Counter = {
+    if (n<= 0) this
+    else  inc.inc(n-1)
+  }
+
+  def dec(n: Int): Counter = {
+    if (n <= 0) this
+    else inc.inc(n - 1)
+  }
+
+  def print = println(count)
+}
+
